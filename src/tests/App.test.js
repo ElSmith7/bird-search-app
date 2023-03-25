395bf41efd6fa615618497b91cb25ab0013d3e2e
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-03-25 18:01:05 +0000

    updated App.test.js
    ensuring the heading and bird search components are visible

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 
-test("displays the App's content", () => {
+test("displays the App's heading", () => {
   render(<App />);
+
+  const heading = screen.getByRole("heading", { name: /bird search/i });
+
+  expect(heading).toBeInTheDocument();
+});
+
+test("displays the search bird form", () => {
+  render(<App />);
+
+  const searchForm = screen.getByText(/search bird/i);
+  expect(searchForm).toBeInTheDocument();
 });
