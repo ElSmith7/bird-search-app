0b6e6f027d4b60a7501a83587afc4d31894e831e
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-04-02 19:08:34 +0100

    Imported msw, updated tests, new test
    new test to check for bird headings rendering correctly

--- src/tests/BirdList.test.js
+++ src/tests/BirdList.test.js
@@ -1,21 +1,22 @@
 import { screen } from "@testing-library/react";
-import { setUpServer } from "msw/node";
+import { setupServer } from "msw/node";
 import { rest } from "msw";
-import { MemoryRouter } from "react-router-dom";
 import { renderWithProviders } from "../utils/utils-for-tests";
 import BirdList from "../components/BirdList";
 
 const handlers = [
   rest.get("/birds", (req, res, ctx) => {
     return res(
-      ctx.json([
-        { name: "blue tit", id: 1 },
-        { name: "grey heron", id: 2 },
-      ])
+      ctx.json({
+        birds: [
+          { name: "blue tit", id: 1 },
+          { name: "grey heron", id: 2 },
+        ],
+      })
     );
   }),
 ];
-const server = setUpServer(...handlers);
+const server = setupServer(...handlers);
 
 beforeAll(() => {
   server.listen();
@@ -28,19 +29,21 @@ afterAll(() => {
 });
 
 test("render the correct number of birds", async () => {
-  const birds = [
-    { name: "blue tit", id: 1 },
-    { name: "grey heron", id: 2 },
-  ];
-  renderWithProviders(
-    <MemoryRouter>
-      <BirdList birds={birds} />
-    </MemoryRouter>
-  );
+  renderWithProviders(<BirdList />);
 
-  const birdList = screen.getAllByTestId("panel");
+  const birdList = await screen.findAllByTestId("panel");
 
   expect(birdList).toHaveLength(2);
 });
 
-test("render the name and numbers");
+test("renders the heading with each bird's name", async () => {
+  renderWithProviders(<BirdList />);
+  const birds = ["blue tit", "grey heron"];
+
+  for (let bird of birds) {
+    const headings = await screen.findByRole("header", {
+      name: new RegExp(`${bird}`),
+    });
+    expect(headings).toHaveTextContent(`${bird}`);
+  }
+});
