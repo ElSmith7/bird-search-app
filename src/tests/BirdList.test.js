f932223288a75bb48ef338879bfd968bb79a01b1
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-04-02 17:08:20 +0100

    update BirdList.test.js
    added msw server to "mock" db.json

--- src/tests/BirdList.test.js
+++ src/tests/BirdList.test.js
@@ -1,15 +1,46 @@
 import { screen } from "@testing-library/react";
+import { setUpServer } from "msw/node";
+import { rest } from "msw";
+import { MemoryRouter } from "react-router-dom";
 import { renderWithProviders } from "../utils/utils-for-tests";
 import BirdList from "../components/BirdList";
 
-test("render the correct number of birds", () => {
+const handlers = [
+  rest.get("/birds", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: "blue tit", id: 1 },
+        { name: "grey heron", id: 2 },
+      ])
+    );
+  }),
+];
+const server = setUpServer(...handlers);
+
+beforeAll(() => {
+  server.listen();
+});
+afterEach(() => {
+  server.resetHandlers();
+});
+afterAll(() => {
+  server.close();
+});
+
+test("render the correct number of birds", async () => {
   const birds = [
     { name: "blue tit", id: 1 },
     { name: "grey heron", id: 2 },
   ];
-  renderWithProviders(<BirdList birds={birds} />);
+  renderWithProviders(
+    <MemoryRouter>
+      <BirdList birds={birds} />
+    </MemoryRouter>
+  );
 
   const birdList = screen.getAllByTestId("panel");
 
   expect(birdList).toHaveLength(2);
 });
+
+test("render the name and numbers");
