ba917a0cc6568bf100a4d513f613afdc895f272e
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-04-22 12:35:22 +0100

    set up sightings state functionality
    Next - make a patch request to store to update number

--- src/components/Sightings.js
+++ src/components/Sightings.js
@@ -1,4 +1,29 @@
+import { useState } from "react";
+import { AiOutlineMinus } from "react-icons/ai";
+import { AiOutlinePlus } from "react-icons/ai";
+import Button from "./Button";
+
 function Sightings({ number }) {
-  return <div>{number}</div>;
+  const [sightings, setSightings] = useState(number);
+
+  const handleMinusSightings = () => {
+    setSightings(sightings - 1);
+  };
+
+  const handleAddSightings = () => {
+    setSightings(sightings + 1);
+  };
+
+  return (
+    <div>
+      <Button symbol onClick={handleMinusSightings}>
+        <AiOutlineMinus />
+      </Button>
+      {sightings}
+      <Button symbol onClick={handleAddSightings}>
+        <AiOutlinePlus />
+      </Button>
+    </div>
+  );
 }
 export default Sightings;
