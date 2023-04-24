0ad40f6b62876a8a597b7e8a452db48afa946a98
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-04-24 14:23:46 +0100

    Api call is functionaing
    Major bug - its making a call to unsplash with every letter entered in "nameInput" and with every letter its also calling the images of all the other birds

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,6 +1,17 @@
-import { useState } from "react";
 import axios from "axios";
 
-async function fetchBirdImg() {}
+async function fetchBirdImg(name) {
+  const response = await axios.get(
+    "https://api.unsplash.com/search/photos?per_page=1&orientation=landscape",
+
+    {
+      headers: {
+        Authorization: "Client-ID 5PsYTLSHQtmbDPMDzBrgVtrP0yT_fZ8s2aoCzLzJa6c",
+      },
+      params: { query: `${name}` },
+    }
+  );
+  console.log(response.data.results);
+}
 
 export { fetchBirdImg };
--- src/components/BirdImg.js
+++ src/components/BirdImg.js
@@ -1,6 +1,7 @@
 import { fetchBirdImg } from "../api/unsplash";
 
 function BirdImg({ name }) {
+  fetchBirdImg(name);
   return <div>{name}</div>;
 }
 
