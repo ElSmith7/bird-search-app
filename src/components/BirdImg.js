bc5f03242bd381196a9020c46b8ed516739b11d6
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-05-01 13:33:31 +0100

    added error img
    added error placeholder img and updated the loading placeholder too

--- src/components/BirdImg.js
+++ src/components/BirdImg.js
@@ -1,10 +1,10 @@
 import { useEffect, useRef, useState } from "react";
 import { fetchBirdImg } from "../api/unsplash";
-import { ClipLoader } from "react-spinners";
 
 function BirdImg({ name }) {
   const imgRef = useRef(null);
   const [loaded, setLoaded] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
@@ -26,10 +26,15 @@ function BirdImg({ name }) {
   }, [name]);
 
   let imgSrc;
+
+  if (loaded === false) {
+    imgSrc = `https://via.placeholder.com/200x300/86efac?text=Loading...`;
+  }
   if (loaded) {
-    imgSrc = { imgRef };
-  } else if (loaded === false) {
-    imgSrc = `https://via.placeholder.com/300x200?text=Loading...`;
+    imgSrc = imgRef.current.src;
+  }
+  if (error === true) {
+    imgSrc = `https://via.placeholder.com/200x300/86efac?text=Error`;
   }
 
   return (
@@ -38,8 +43,12 @@ function BirdImg({ name }) {
       onLoad={() => {
         setLoaded(true);
       }}
+      onError={() => {
+        setError(true);
+      }}
       src={imgSrc}
       alt={name}
+      className="w-20"
     />
   );
 }
