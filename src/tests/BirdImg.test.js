82379f31f555754e80e3e212a4a57c5750a2bbfd
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-05-06 17:13:08 +0100

    updated test but it is still failing.
    Have asked for help on stack overflow. Will do some research and come back to it.

--- src/tests/BirdImg.test.js
+++ src/tests/BirdImg.test.js
@@ -2,7 +2,6 @@ import { screen, waitFor } from "@testing-library/react";
 import { renderWithProviders } from "../utils/utils-for-tests";
 import axios from "axios";
 import BirdImg from "../components/BirdImg";
-import { fetchBirdImg } from "../api/unsplash";
 
 class IntersectionObserver {
   observe() {
@@ -19,9 +18,6 @@ window.IntersectionObserver = IntersectionObserver;
 jest.mock("axios", () => ({
   get: jest.fn(),
 }));
-jest.mock("../api/unsplash", () => ({
-  fetchBirdImg: jest.fn(),
-}));
 
 const mockLoadedResponse = {
   data: {
@@ -37,33 +33,14 @@ const mockLoadedResponse = {
 test("shows the image", async () => {
   axios.get.mockResolvedValue(mockLoadedResponse);
 
-  renderWithProviders(<BirdImg />);
+  renderWithProviders(<BirdImg name="robin" />);
   const birdImage = await screen.findByRole("img");
-  console.log(birdImage);
-  expect(birdImage).toBeInTheDocument();
-});
-describe("fetchBirdImg", () => {
-  describe("when api call is successful", () => {
-    it("should return the correct bird img", async () => {
-      fetchBirdImg.mockResolvedValue(mockLoadedResponse);
 
-      renderWithProviders(<BirdImg name="robin" />);
-
-      const birdImage = screen.queryByRole("img");
-      expect(birdImage.src).toBe(
-        "https://via.placeholder.com/200x300/86efac?text=Loading..."
-      );
-      await waitFor(
-        () => {
-          expect(birdImage.src).not.toBe(
-            "https://via.placeholder.com/200x300/86efac?text=Loading..."
-          );
-          expect(birdImage.src).toBe(
-            mockLoadedResponse.data.results[0].urls.thumb
-          );
-        },
-        { timeout: 4000 }
-      );
-    });
+  await waitFor(() => {
+    expect(birdImage).toBeInTheDocument();
+    expect(birdImage).toHaveAttribute(
+      "src",
+      "https://via.placeholder.com/300x300.png?text=robin"
+    );
   });
 });
