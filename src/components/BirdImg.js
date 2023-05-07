a35be58febd92fbba45ca05b7a077a3e5029242b
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-05-07 13:44:28 +0100

    testing to see what's causing the tests to fail

--- src/components/BirdImg.js
+++ src/components/BirdImg.js
@@ -1,56 +1,73 @@
-import { useEffect, useRef, useState } from "react";
+import { useState, useEffect } from "react";
 import { fetchBirdImg } from "../api/unsplash";
-
 function BirdImg({ name }) {
-  const imgRef = useRef(null);
-  const [loaded, setLoaded] = useState(false);
-  const [error, setError] = useState(false);
-
+  const [imgUrl, setImgUrl] = useState("");
   useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          fetchBirdImg(name).then((data) => {
-            imgRef.current.src = data[0].urls.thumb;
-            setLoaded(true);
-          });
-
-          observer.unobserve(imgRef.current);
-        }
-      });
-    });
-
-    observer.observe(imgRef.current);
-
-    return () => observer.disconnect();
-  }, [name]);
-
-  let imgSrc;
-
-  if (loaded === false) {
-    imgSrc = `https://via.placeholder.com/200x300/86efac?text=Loading...`;
-  }
-  if (loaded) {
-    imgSrc = imgRef.current.src;
-  }
-  if (error === true) {
-    imgSrc = `https://via.placeholder.com/200x300/86efac?text=Error`;
-  }
-
-  return (
-    <img
-      ref={imgRef}
-      onLoad={() => {
-        setLoaded(true);
-      }}
-      onError={() => {
-        setError(true);
-      }}
-      src={imgSrc}
-      alt={name}
-      className="w-20"
-    />
-  );
+    async function fetchImg() {
+      const results = await fetchBirdImg(name);
+      if (results.length > 0) {
+        setImgUrl(results[0].urls.thumb);
+      }
+    }
+    fetchImg();
+  }, []);
+  return <img src={imgUrl} alt={`${name} bird`} />;
 }
-
 export default BirdImg;
+
+// import { useEffect, useRef, useState } from "react";
+// import { fetchBirdImg } from "../api/unsplash";
+
+// function BirdImg({ name }) {
+//   const imgRef = useRef(null);
+//   const [loaded, setLoaded] = useState(false);
+//   const [error, setError] = useState(false);
+
+//   useEffect(() => {
+//     const observer = new IntersectionObserver((entries) => {
+//       entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//           fetchBirdImg(name).then((data) => {
+//             imgRef.current.src = data[0].urls.thumb;
+//             setLoaded(true);
+//           });
+
+//           observer.unobserve(imgRef.current);
+//         }
+//       });
+//     });
+
+//     observer.observe(imgRef.current);
+
+//     return () => observer.disconnect();
+//   }, [name]);
+
+//   let imgSrc;
+
+//   if (loaded === false) {
+//     imgSrc = `https://via.placeholder.com/200x300/86efac?text=Loading...`;
+//   }
+//   if (loaded) {
+//     imgSrc = imgRef.current.src;
+//   }
+//   if (error === true) {
+//     imgSrc = `https://via.placeholder.com/200x300/86efac?text=Error`;
+//   }
+
+//   return (
+//     <img
+//       ref={imgRef}
+//       onLoad={() => {
+//         setLoaded(true);
+//       }}
+//       onError={() => {
+//         setError(true);
+//       }}
+//       src={imgSrc}
+//       alt={name}
+//       className="w-20"
+//     />
+//   );
+// }
+
+// export default BirdImg;
--- src/tests/BirdImg.test.js
+++ src/tests/BirdImg.test.js
@@ -1,19 +1,8 @@
 import { screen, waitFor } from "@testing-library/react";
 import { renderWithProviders } from "../utils/utils-for-tests";
 import axios from "axios";
+import { useInView } from "react-intersection-observer";
 import BirdImg from "../components/BirdImg";
-
-class IntersectionObserver {
-  observe() {
-    return null;
-  }
-
-  disconnect() {
-    return null;
-  }
-}
-
-window.IntersectionObserver = IntersectionObserver;
 
 jest.mock("axios", () => ({
   get: jest.fn(),
@@ -30,8 +19,20 @@ const mockLoadedResponse = {
     ],
   },
 };
+
 test("shows the image", async () => {
   axios.get.mockResolvedValue(mockLoadedResponse);
+
+  // let intersectionCallback;
+  // const observeMock = jest.fn().mockImplementation((callback) => {
+  //   intersectionCallback = callback;
+  // });
+  // const disconnectMock = jest.fn();
+
+  // window.IntersectionObserver = jest.fn((callback) => ({
+  //   observe: observeMock,
+  //   disconnect: disconnectMock,
+  // }));
 
   renderWithProviders(<BirdImg name="robin" />);
   const birdImage = await screen.findByRole("img");
@@ -43,4 +44,71 @@ test("shows the image", async () => {
       "https://via.placeholder.com/300x300.png?text=robin"
     );
   });
+
+  // intersectionCallback([{ isIntersecting: true, target: birdImage }]);
 });
+
+// import { screen, waitFor } from "@testing-library/react";
+// import { renderWithProviders } from "../utils/utils-for-tests";
+// import axios from "axios";
+// import BirdImg from "../components/BirdImg";
+
+// jest.mock("axios", () => ({
+//   get: jest.fn(),
+// }));
+
+// const mockLoadedResponse = {
+//   data: {
+//     results: [
+//       {
+//         urls: {
+//           thumb: "https://via.placeholder.com/300x300.png?text=robin",
+//         },
+//       },
+//     ],
+//   },
+// };
+
+// test("shows the image", async () => {
+//   axios.get.mockResolvedValue(mockLoadedResponse);
+//   const observeMock = jest.fn().mockImplementation((callback) => {
+//     const entry = {
+//       isIntersecting: true,
+//       target: {
+//         getBoundingClientRect: () => ({
+//           bottom: 0,
+//           height: 0,
+//           left: 0,
+//           right: 0,
+//           top: 0,
+//           width: 0,
+//         }),
+//       },
+//     };
+//     callback([entry]);
+//   });
+//   const disconnectMock = jest.fn();
+//   const callback = jest.fn((entries) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         observeMock(entry.target);
+//       }
+//     });
+//   });
+
+//   window.IntersectionObserver = jest.fn((callback) => ({
+//     observe: observeMock,
+//     disconnect: disconnectMock,
+//   }));
+
+//   renderWithProviders(<BirdImg name="robin" />);
+//   const birdImage = await screen.findByRole("img");
+
+//   await waitFor(() => {
+//     expect(birdImage).toBeInTheDocument();
+//     expect(birdImage).toHaveAttribute(
+//       "src",
+//       "https://via.placeholder.com/300x300.png?text=robin"
+//     );
+//   });
+// });
