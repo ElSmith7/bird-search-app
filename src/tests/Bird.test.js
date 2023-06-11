cc312510d748c013c3f6a57c141ec2cbf0023f10
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-06-11 14:54:19 +0100

    Moved sighting unit test into Bird.test
    This keeps all the unit tests for the bird component together, making them a bit easier to find

--- src/tests/Bird.test.js
+++ src/tests/Bird.test.js
@@ -15,34 +15,28 @@ jest.mock("../components/BirdImg", () => {
   };
 });
 
-jest.mock("../components/Sightings", () => {
-  return () => {
-    return "Sightings Component";
-  };
-});
+// const handlers = [
+//   rest.get("http://localhost:3005/birds", (req, res, ctx) => {
+//     return res(
+//       ctx.json([
+//         { id: "1", name: "blue tit", number: "5" },
+//         { id: "2", name: "grey heron", number: "1" },
+//       ])
+//     );
+//   }),
+// ];
+// const server = setupServer(...handlers);
 
-const handlers = [
-  rest.get("http://localhost:3005/birds", (req, res, ctx) => {
-    return res(
-      ctx.json([
-        { id: "1", name: "blue tit", number: "5" },
-        { id: "2", name: "grey heron", number: "1" },
-      ])
-    );
-  }),
-];
-const server = setupServer(...handlers);
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+// beforeAll(() => server.listen());
+// afterEach(() => server.resetHandlers());
+// afterAll(() => server.close());
 
 const bird = {
   id: "1",
   name: "blue tit",
   number: "5",
 };
-test("shows bird name, sightings, remove button and bird image", async () => {
+test("shows bird name, remove button and bird image", async () => {
   renderWithProviders(<Bird bird={bird} />);
 
   expect(
@@ -50,13 +44,22 @@ test("shows bird name, sightings, remove button and bird image", async () => {
       name: "blue tit",
     })
   ).toBeInTheDocument();
-  expect(await screen.findByText("Sightings Component")).toBeInTheDocument();
   expect(
     await screen.findByTestId(`removeButton-${bird.id}`)
   ).toBeInTheDocument();
   expect(await screen.findByText("Bird Img Component")).toBeInTheDocument();
 });
+test("it shows the number of sightings, and two buttons", async () => {
+  renderWithProviders(<Bird bird={bird} />);
+  const sightings = await screen.findByTestId(`${bird.number}`);
+  const minusButton = await screen.findByTestId("minus-1");
+  const plusButton = await screen.findByTestId("plus-1");
 
+  expect(sightings).toBeInTheDocument();
+  expect(screen.getByText("5")).toBeInTheDocument();
+  expect(minusButton).toBeInTheDocument();
+  expect(plusButton).toBeInTheDocument();
+});
 test("shows modal before removal", async () => {
   renderWithProviders(<Bird bird={bird} />);
   const removeButton = await screen.findByTestId(`removeButton-${bird.id}`);
