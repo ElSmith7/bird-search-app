73a7c46374532b23700b3973463f8008fc717f2e
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-07-06 16:46:50 +0100

    Updated SearchBird.js style
    Made changes to account for phone users.

--- src/components/SearchBird.js
+++ src/components/SearchBird.js
@@ -72,29 +72,30 @@ function SearchBird() {
 
   return (
     <>
-      <Panel primary className="bird-search mb-5">
+      <Panel primary className="bird-search mb-6 pt-0">
         <form
           className="md:flex md:flex-row md:flex-wrap md:justify-between"
           onSubmit={handleSubmit}
         >
-          <div className="md:basis-2/5">
+          <div className="mt-4 md:mt-0 md:basis-2/5">
             <label
-              className="block font-bold text-slate-700/75"
+              className="block font-bold text-slate-700/75 md:invisible"
               htmlFor="search"
             >
-              New Bird
+              Bird Name
             </label>
             <input
               id="search"
               value={name}
               type="text"
+              placeholder="European Robin"
               onChange={handleNameChange}
-              className="border border-2 block w-full mt-2 text-gray-700 rounded py-2 px-3 focus:outline-none focus:border-gray-300 focus:drop-shadow"
+              className="border border-2 block w-full mt-2 rounded py-2 px-3 text-gray-700 placeholder-gray-700 placeholder-opacity-0 focus:outline-none focus:border-gray-300 focus:drop-shadow md:placeholder-opacity-25"
             />
           </div>
           <div className="mt-4 md:mt-0 md:basis-2/5">
             <label
-              className="block font-bold text-slate-700/75"
+              className="block font-bold text-slate-700/75 md:invisible"
               htmlFor="number"
             >
               Number Seen
@@ -104,22 +105,27 @@ function SearchBird() {
               type="number"
               value={number || ""}
               min="1"
+              placeholder="2"
               onChange={handleNumberChange}
               onPaste={preventPasteNegative}
               onKeyDown={preventMinus}
-              className="border border-2 block w-full mt-2 text-gray-700 rounded py-2 px-3 focus:outline-none focus:border-gray-300 focus:drop-shadow"
+              className="border border-2 block w-full mt-2 rounded py-2 px-3 text-gray-700 placeholder-gray-700 placeholder-opacity-0 focus:outline-none focus:border-gray-300 focus:drop-shadow md:placeholder-opacity-25"
             />
           </div>
+
           <div className="grid place-content-center mt-7 md:basis-1/6">
             <Button
               primary
-              className="px-10 md:px-3"
+              className="px-16 md:px-3"
               loading={addBirdResults.isLoading}
             >
               Add
             </Button>
           </div>
         </form>
+        <h6 className="hidden mt-1 mb-2 text-xs text-slate-700/50 md:block">
+          Add a new bird and the number you've seen or search your list!
+        </h6>
       </Panel>
       {showModal && modal}
     </>
